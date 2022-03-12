982f2fbc1b9da48ccc5351c6568d8d102669aab1
Mahmoud Marayef <mahmoudmarayef@gmail.com>
2022-03-12 15:08:46 +0200

    Create the base components and the footer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDom from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import 'antd/dist/antd.css';
 
 ReactDom.render(<Router><App /></Router>, document.getElementById('root'));
