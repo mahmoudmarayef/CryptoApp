9020c3ee139298b7a16136337961c6385b4903d2
Mahmoud Marayef <mahmoudmarayef@gmail.com>
2022-03-12 19:15:38 +0200

    Fetch apis and create Homepage component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import store from "./app/store";
 import 'antd/dist/antd.css';
 
-ReactDom.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDom.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>, 
+    document.getElementById('root')
+);
